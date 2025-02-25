68a49dd74f4796fe96f43ac433a5ef3b5ecebee3
Muskan <sharmamuskan17017@gmail.com>
2025-02-25 13:30:54 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -5,12 +5,16 @@ import dotenv from "dotenv";
 
 dotenv.config();
 const app = express();
+const port=process.env.PORT||8002
 
 // Database connection
 dbConnect();
 
 app.use(express.json());
-app.post("/register", Register);
+app.post("/api/register", Register);
 
 // Export the app as a serverless function
+app.listen(port,()=>{
+    console.log("server run",port)
+})
 export default app;
